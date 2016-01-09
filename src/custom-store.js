d17f52a25b28a009a571fd666daa3626ffde2233
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2016-01-09 14:41:43 +0530

    Custom implementation of the basic Redux Store functions

---
+++ src/custom-store.js
@@ -0,0 +1,47 @@
+(() => {
+  'use strict';
+
+  const
+    counter = require('./reducer').counter,
+
+    createStore = (reducer) => {
+      let
+        state,
+        listeners = [];
+
+      const
+        getState = () => state,
+
+        dispatch = (action) => {
+          state = reducer(state, action);
+
+          listeners.forEach((listener) => listener());
+        },
+
+        subscribe = (listener) => {
+          listeners.push(listener);
+
+          return () => {
+            listeners = listeners.filter((l) => l !== listener);
+          };
+        };
+
+      dispatch({});
+
+      return { getState, dispatch, subscribe };
+    },
+
+    store = createStore(counter),
+
+    render = () => {
+      document.body.textContent = store.getState();
+    };
+
+  store.subscribe(render);
+  render();
+
+  document.addEventListener('click', () => {
+    store.dispatch({ 'type': 'INC' });
+  });
+
+})();
