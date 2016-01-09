3a90de886085934c239b67fb1d09115b3a0e2663
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2016-01-09 11:32:45 +0530

    Added test support via mocha and chai

---
+++ test/test.js
@@ -0,0 +1,11 @@
+(() => {
+  'use strict';
+
+  const expect = require('chai').expect;
+
+  describe('Sample test', () => {
+    it('1 + 1 = 2', () => {
+      expect(1 + 1).to.equal(2);
+    });
+  });
+})();
