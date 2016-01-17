b558e670156c0f1f25f6da3ba3f7229d9a803a86
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2016-01-17 15:17:37 +0530

    Presentational Components: AddTodo, Footer, FilterLink

--- src/todo-redux-store-react.js
+++ src/todo-redux-store-react.js
@@ -20,6 +20,23 @@
       return todos;
     },
 
+    AddTodo = ({ onAddClick }) => {  // eslint-disable-line no-unused-vars
+      let input;
+
+      return <div>
+        <input ref={(node) => {
+          input = node;
+        }} />
+        <button
+          onClick={() => {
+            onAddClick(input.value);
+
+            input.value = '';
+          }}
+        >Add Todo</button>
+      </div>;
+    },
+
     Todo = ({ text, completed, onClick }) => {  // eslint-disable-line no-unused-vars
       return <li
         onClick={onClick}
@@ -40,48 +57,7 @@
       </ul>;
     },
 
-    TodoApp = ({ todos, visibilityFilter }) => {  // eslint-disable-line no-unused-vars
-      const visibleTodos = getVisibleTodos(todos, visibilityFilter);
-
-      return <div>
-        <input ref={(node) => {
-          this.input = node;
-        }} />
-        <button
-          onClick={() => {
-            store.dispatch({
-              'type': 'ADD_TODO',
-              'text': this.input.value,
-              'id': nextTodoId += 1
-            });
-
-            this.input.value = '';
-          }}
-        >Add Todo</button>
-
-        <TodoList
-          todos={visibleTodos}
-          onTodoClick={(id) => {
-            store.dispatch({
-              'type': 'TOGGLE_TODO',
-              id
-            });
-          }}
-        />
-
-        <p>
-          Show:
-          {' '}
-          <FilterLink filter='SHOW_ALL' currentFilter={visibilityFilter}>All</FilterLink>
-          {' '}
-          <FilterLink filter='SHOW_ACTIVE' currentFilter={visibilityFilter}>Active</FilterLink>
-          {' '}
-          <FilterLink filter='SHOW_COMPLETED' currentFilter={visibilityFilter}>Completed</FilterLink>
-        </p>
-      </div>;
-    },
-
-    FilterLink = ({ filter, currentFilter, children }) => {  // eslint-disable-line no-unused-vars
+    FilterLink = ({ filter, currentFilter, children, onClick }) => {  // eslint-disable-line no-unused-vars
       if(filter === currentFilter) {
         return <span>{children}</span>;
       }
@@ -90,12 +66,56 @@
         onClick={(e) => {
           e.preventDefault();
 
-          store.dispatch({
-            'type': 'SET_VISIBILITY_FILTER',
-            filter
-          });
+          onClick(filter);
         }}
         >{children}</a>;
+    },
+
+    Footer = ({ visibilityFilter, onFilterClick }) => {  // eslint-disable-line no-unused-vars
+      return <p>
+        Show:
+        {' '}
+        <FilterLink filter='SHOW_ALL' currentFilter={visibilityFilter} onClick={onFilterClick}>All</FilterLink>
+        {' '}
+        <FilterLink filter='SHOW_ACTIVE' currentFilter={visibilityFilter} onClick={onFilterClick}>Active</FilterLink>
+        {' '}
+        <FilterLink filter='SHOW_COMPLETED' currentFilter={visibilityFilter} onClick={onFilterClick}>Completed</FilterLink>
+      </p>;
+    },
+
+    TodoApp = ({ todos, visibilityFilter }) => {  // eslint-disable-line no-unused-vars
+      return <div>
+        <AddTodo
+          onAddClick={(text) => {
+            store.dispatch({
+              'type': 'ADD_TODO',
+              'id': nextTodoId += 1,
+              text
+            });
+          }}
+        />
+
+        <TodoList
+          todos={getVisibleTodos(todos, visibilityFilter)}
+          onTodoClick={(id) => {
+            store.dispatch({
+              'type': 'TOGGLE_TODO',
+              id
+            });
+          }}
+        />
+
+        <Footer
+          visibilityFilter={visibilityFilter}
+          onFilterClick={(filter) => {
+            store.dispatch({
+              'type': 'SET_VISIBILITY_FILTER',
+              filter
+            });
+          }}
+        />
+
+      </div>;
     },
 
     render = () => {
